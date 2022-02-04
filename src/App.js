8d5ed589f65eebd6df90f078a0644a47809b33e9
mkreive <mkreive@gmail.com>
2022-02-04 10:08:16 +0200

    managing users list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
+    const [usersList, setUsersList] = useState([]);
+
+    const addUserHandler = (uName, uAge, uId) => {
+        setUsersList((prevUsersList) => {
+            return [
+                ...prevUsersList,
+                { name: uName, age: uAge, id: Math.random().toString() },
+            ];
+        });
+    };
+
+    console.log(usersList);
+
     return (
         <div>
-            <AddUser />
-            <UsersList users={[]} />
+            <AddUser onAddUser={addUserHandler} />
+            <UsersList users={usersList} />
         </div>
     );
 }
