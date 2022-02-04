41d3c2183a7c8ab066eaa16227d63873d3ab7ed3
mkreive <mkreive@gmail.com>
2022-02-04 10:47:03 +0200

    adding errormodal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState("");
@@ -34,25 +35,31 @@ const AddUser = (props) => {
     };
 
     return (
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input
-                    id="username"
-                    type="text"
-                    onChange={usernameChangeHandler}
-                    value={enteredUsername}
-                ></input>
-                <label htmlFor="age">Age (Years)</label>
-                <input
-                    id="age"
-                    type="number"
-                    onChange={ageChangeHandler}
-                    value={enteredAge}
-                ></input>
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
+        <div>
+            <ErrorModal
+                title="An error occured!"
+                message="Something went wrong!"
+            />
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        id="username"
+                        type="text"
+                        onChange={usernameChangeHandler}
+                        value={enteredUsername}
+                    ></input>
+                    <label htmlFor="age">Age (Years)</label>
+                    <input
+                        id="age"
+                        type="number"
+                        onChange={ageChangeHandler}
+                        value={enteredAge}
+                    ></input>
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </div>
     );
 };
 
